df3dc69b6eafe5deda02e7ed8eb39d9f8a96d829
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2023-05-10 21:35:14 -0400

    infra(ctOS): remove old folder

--- ctOS/driver/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Package `driver` contains classes and functions used by `ctOS`-based programs to
-communicate with the outside world. This includes, for instance, things like
-requesting and receiving symbol data, fetching account information, placing
-market orders, etc.
-"""
--- ctOS/std/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Package `std` contains types that are widely recognized as the de facto standard
-by `ctos` and its plugins.
-"""
